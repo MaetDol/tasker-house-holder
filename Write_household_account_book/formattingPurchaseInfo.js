59e46b15a0e997a06f5413f08514ad733d979929
Maetdol <rndka9@gmail.com>
2020-11-05 00:46:46 +0900

    Fix recoreded yesterday by parallax
    시차계산을 안해서 가끔 전날 날짜로 기록되는 문제를 시차만큼 더해서 수정

--- Write_household_account_book/formattingPurchaseInfo.js
+++ Write_household_account_book/formattingPurchaseInfo.js
@@ -1,7 +1,10 @@
-var todayDate = new Date().getUTCDate() + ''
-var updatedDate = global('UHAB_updatedDate')
+const GMT_PARALLAX = 9 * 60*60*1000;
+var todayDate = new Date( new Date() + GMT_PARALLAX )
+	.getUTCDate()
+	.toString();
+var updatedDate = global('UHAB_updatedDate');
 flash(`${todayDate}, ${updatedDate}`);
-if( updatedDate == null || updatedDate != todayDate ) {
+if( updatedDate !== todayDate ) {
 	setGlobal( 'UHAB_updatedDate', todayDate )
 	todayDate = `¶™¶${todayDate}™`;
 } else {
