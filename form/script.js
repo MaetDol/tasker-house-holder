bfc119ad580b25ed52d8d8a2eb3bf24d3e87815a
MaetDol <rndka9@gmail.com>
2021-02-04 18:07:46 +0900

    Add event listener when document not loaded
    Because this script load dynamically,
    So it can be load after 'document complete'

--- form/script.js
+++ form/script.js
@@ -1,11 +1,16 @@
+if( document.readyState === 'loading' ){
+  window.addEventListener('DOMContentLoaded', init);
+} else {
+  init();
+}
+
 const $ = s => document.querySelectorAll(s);
 
-document.addEventListener('DOMContentLoaded', _ => {
-
+function init(){
   $('input').forEach( el => {
     el.oninput = _=> setLocal( el.name, el.value );
   });
 
   const sceneName = $('body')[0].dataset.sceneName;
   $('button')[0].onclick = _ => destroyScene( sceneName );
-});
+}
