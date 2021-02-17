00b5a263043930563bfcf5bf920f7697915e0d2f
MaetDol <rndka9@gmail.com>
2021-02-18 01:36:49 +0900

    Rename global variable name of GLOBAL_UPDATED_DATE
    다른 변수명과 같이 컨벤션을 맞추고
    명확하게 하기위해 수정했습니다.

--- scripts/constant.js
+++ scripts/constant.js
@@ -3,7 +3,7 @@ const FILE_PATH = FILE_DIR + 'storeList';
 
 const GMT_PARALLAX = 9 * 60*60**1000;
 
-const GLOBAL_UPDATED_DATE = 'UHAB_updatedDate';
+const GLOBAL_UPDATED_DATE = 'UHAB_last_date';
 const GLOBAL_NOTIFY = 'UHAB_last_notify';
 const GLOBAL_SHEET_LINK = 'UHAB_spreadsheet_url';
 
