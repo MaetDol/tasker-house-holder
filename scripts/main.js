ee1eefb83a34b7e207517ca720e6c51aff890c5e
MaetDol <rndka9@gmail.com>
2021-02-18 01:43:20 +0900

    Fix misuse of returned store data
    Data 객체를 반환하는데,
    일반 오브젝트처럼 접근해서 정보를 얻을 수 없었습니다.
    반환받는 변수의 이름을 바꾸고 불필요한 변수 선언을 정리했습니다.

--- scripts/main.js
+++ scripts/main.js
@@ -17,20 +17,18 @@ if( notifyInfo ) {
 }
 
 // 가게 정보가 있다면 해당 정보로 시트 작성
-const store = getStore( purchase.data.get('store') );
-if( store ) {
-  const price = purchase.data.get('price');
-  const memo = store.memo || store.store;
+const storeData = getStore( purchase.data.get('store') );
+if( storeData ) {
   const data = new Data({ 
-    price,
-    type: store.type,
-    memo
+    price: purchase.data.get('price'),
+    type: storeData.get('type'),
+    memo: storeData.get('memo'),
   });
 
   writeSheet( data );
   notify({
     title: '메모 완료!',
-    text: `${memo}에서 ${price}원을 결제하셨네요. 메모 해두겠습니다.`
+    text: `${data.get('memo')}에서 ${data.get('price')}원을 결제하셨네요. 기록해둘게요!`
   });
 } else {
   purchase.data.set('type', '기타');
