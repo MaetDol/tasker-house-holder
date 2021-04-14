439c58eea2c57cb2e066bb72d5d70c828fa1b536
MaetDol <rndka9@gmail.com>
2021-04-14 12:39:54 +0900

    Refactor: Delete unnecessary comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 function main( sms ) {
-  // 결제 확인 문자인지 확인
   const purchase = new Purchase( sms, ShinhanCheckParser );
   if( purchase.isNot ) exit();
 
@@ -15,14 +14,11 @@ function main( sms ) {
 }
 
 function createStoreFile() {
-  // 가게 정보를 담는 파일의 상위경로가 없다면 생성
   if( !isDirExists( FILE_DIR ) ) createDirectory( FILE_DIR );
-  // 파일이 존재하는지 확인하는 것 보단 작성하는게 더 저렴할 지도 모른다
   writeTo( FILE_PATH, '' );
 }
 
 function flushPreviousNotification() {
-  // 알림이 띄워진 상태면 시트에 그대로 작성
   const notifyInfo = global( GLOBAL_NOTIFY );
   if( notifyInfo ) {
     const notifyData = Data.fromNotifyFormat( notifyInfo );
