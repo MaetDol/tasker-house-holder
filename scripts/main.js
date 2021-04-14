76afeea9009b02714a5ca9d94cdf36f03ec311ec
MaetDol <rndka9@gmail.com>
2021-04-14 12:37:41 +0900

    Refactor: 기능별로 함수로 묶어둠
    후에 디버깅이나 테스팅 용이하게 함수로 묶어두었다
    main.js 스크립트를 불러올 뿐만 아니라 main 함수를
    실행해줘야 등록절차가 시작된다. 인자로 문자내용을 받는다

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,45 @@
-// 결제 확인 문자인지 확인
-const sms = global('SMSRB');
-const purchase = new Purchase( sms, ShinhanCheckParser );
-if( purchase.isNot ) exit();
+function main( sms ) {
+  // 결제 확인 문자인지 확인
+  const purchase = new Purchase( sms, ShinhanCheckParser );
+  if( purchase.isNot ) exit();
 
-// 가게 정보를 담는 파일의 상위경로가 없다면 생성
-if( !isDirExists( FILE_DIR ) ) createDirectory( FILE_DIR );
-// 파일이 존재하는지 확인하는 것 보단 작성하는게 더 저렴할 지도 모른다
-writeTo( FILE_PATH, '' );
+  createStoreFile();
+  flushPreviousNotification();
 
-// 알림이 띄워진 상태면 시트에 그대로 작성
-const notifyInfo = global( GLOBAL_NOTIFY );
-if( notifyInfo ) {
-  const notifyData = Data.fromNotifyFormat( notifyInfo );
-  writeSheet( notifyData );
+  const storeData = getStore( purchase.data.get('store') );
+  if( storeData ) writePurchaseInfo( storeData );
+  else {
+    purchase.data.set('type', '기타');
+    notifyNewStore( purchase.data );
+  }
 }
 
-// 가게 정보가 있다면 해당 정보로 시트 작성
-const storeData = getStore( purchase.data.get('store') );
-if( storeData ) {
+function createStoreFile() {
+  // 가게 정보를 담는 파일의 상위경로가 없다면 생성
+  if( !isDirExists( FILE_DIR ) ) createDirectory( FILE_DIR );
+  // 파일이 존재하는지 확인하는 것 보단 작성하는게 더 저렴할 지도 모른다
+  writeTo( FILE_PATH, '' );
+}
+
+function flushPreviousNotification() {
+  // 알림이 띄워진 상태면 시트에 그대로 작성
+  const notifyInfo = global( GLOBAL_NOTIFY );
+  if( notifyInfo ) {
+    const notifyData = Data.fromNotifyFormat( notifyInfo );
+    writeSheet( notifyData );
+  }
+}
+
+function writePurchaseInfo( data ) {
   const data = new Data({ 
     price: purchase.data.get('price'),
     type: storeData.get('type'),
     memo: storeData.get('memo'),
   });
 
-  writeSheet( data );
   notify({
     title: '메모 완료!',
     text: `${data.get('memo')}에서 ${data.get('price')}원을 결제하셨네요. 기록해둘게요!`
   });
-} else {
-  purchase.data.set('type', '기타');
-  notifyNewStore( purchase.data );
+  writeSheet( data );
 }
