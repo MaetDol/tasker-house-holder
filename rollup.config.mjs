7667b438fe3b59de9397fa2a8eb3489248988101
MaetDol <rndka9@gmail.com>
2024-06-29 12:51:50 +0900

    [#15] 각 프로파일용 엔트리 스크립트 파일 빌드하게 변경

---
+++ rollup.config.mjs
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const entryDir = path.resolve(__dirname + "/scripts/entries");
+const entryFiles = fs.readdirSync(entryDir);
+
+console.log(entryFiles);
+
+const entryBuildOptions = entryFiles.map((file) => ({
+  input: path.resolve(entryDir, file),
+  output: {
+    dir: "dist/",
+    format: "es",
+  },
+}));
+
+export default entryBuildOptions;
